6e7f7b65f36114c75df96dcea5f25c944037e7ee
Constantin Hong <hongconstantin@gmail.com>
2023-03-29 02:00:16 +0900

    startGH_git/add: Init

---
+++ appendGH_git.sh
@@ -0,0 +1,28 @@
+function ghcreatepri(){
+gh repo create $@ --private
+}
+
+function ghaddremote(){
+if [ -z $1 ]
+then
+    echo "please provide a remote repo"
+    return
+fi
+set -x
+# if there is no remote repo, then add $1 to a remote repo called origin.
+git remote show origin || git remote add origin git@github.com:$1.git && git push --set-upstream origin master
+set +x
+
+}
+
+set -x
+
+gitproject=$1
+REMOTEURL=$(ghcreatepri $gitproject | sed 's/https:\/\/github.com\/\(.*\)/git@github.com:\1.git/')
+echo $REMOTEURL
+
+GITWORKDIR=$PWD/$gitproject
+GITDIR=$PWD/$gitproject/.git
+git --work-tree=$GITWORKDIR --git-dir=$GITDIR remote add origin $REMOTEURL
+git --work-tree=$GITWORKDIR --git-dir=$GITDIR push --set-upstream origin master
+set +x
---
+++ startGH_git.sh
@@ -0,0 +1,30 @@
+function ghcreatepri(){
+gh repo create $@ --private
+}
+
+function ghaddremote(){
+if [ -z $1 ]
+then
+    echo "please provide a remote repo"
+    return
+fi
+set -x
+# if there is no remote repo, then add $1 to a remote repo called origin.
+git remote show origin || git remote add origin git@github.com:$1.git && git push --set-upstream origin master
+set +x
+
+}
+
+set -x
+
+gitproject=$1
+REMOTEURL=$(ghcreatepri $gitproject | sed 's/https:\/\/github.com\/\(.*\)/git@github.com:\1.git/')
+echo $REMOTEURL
+
+git init $gitproject
+GITWORKDIR=$PWD/$gitproject
+GITDIR=$PWD/$gitproject/.git
+git --work-tree=$GITWORKDIR --git-dir=$GITDIR remote add origin $REMOTEURL
+# cannot push new empty git repo
+#git --work-tree=$GITWORKDIR --git-dir=$GITDIR push --set-upstream origin master
+set +x
